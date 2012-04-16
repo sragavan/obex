sragavan/obex

commit c07ddfbd019d3545cce2d7ec694143cdc55a2167

client: free active transfer on session shutdown
The currently active request should be canceled just like any other
queued transfer. Otherwise obex timeout is reported.

client/session.c
@@ -496,6 +496,14 @@ void obc_session_shutdown(struct obc_session *session)
 	err = g_error_new(OBEX_IO_ERROR, OBEX_IO_DISCONNECTED,
 						"Session closed by user");
 
+	if (session->p != NULL) {
+		if (session->p->func)
+			session->p->func(session, err, session->p->data);
+
+		pending_request_free(session->p);
+		session->p = NULL;
+	}
+
 	while ((p = g_queue_pop_head(session->queue))) {
 		if (p->func)
 			p->func(session, err, p->data);

commit cde59cc6c7785dadc98d0ab24bfb5ba915d200d0

client: Fix regression when calling obc_session_shutdown within callback
Commit c07ddfbd019d3545cce2d7ec694143cdc55a2167 introduced the freeing of
the active pending request on obc_session_shutdown without checking if
the request was already processed/terminated.

client/session.c
@@ -494,7 +494,7 @@ void obc_session_shutdown(struct obc_session *session)
 	err = g_error_new(OBEX_IO_ERROR, OBEX_IO_DISCONNECTED,
 						"Session closed by user");
 
-	if (session->p != NULL) {
+	if (session->p != NULL && session->p->id != 0) {
 		if (session->p->func)
 			session->p->func(session, err, session->p->data);
 
@@ -820,6 +820,8 @@ static void session_terminate_transfer(struct obc_session *session,
 		p = match->data;
 		g_queue_delete_link(session->queue, match);
 	}
+
+	p->id = 0;
 
 	obc_session_ref(session);
 